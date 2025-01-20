ebf9e6916501abf35e8066f2ba6129d4da711229
GiraMorales <gira.morales@gamil.com>
2025-01-20 13:49:49 +0100

    2

--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -1,3 +1,4 @@
+const { updateProjectRelations } = require('../controllers/project6');
 const {
   postUser,
   getUsers,
@@ -10,6 +11,7 @@ const UserRoutes = require('express').Router();
 UserRoutes.post('/', postUser); // Crear usuario
 UserRoutes.get('/', getUsers); // Obtener todos los usuarios
 UserRoutes.put('/:id', updateUser); // Actualizar usuario por ID
+UserRoutes.put('/update-relation/:id', updateProjectRelations);
 UserRoutes.delete('/:id', deleteUser); // Eliminar usuario por ID
 
 module.exports = UserRoutes; // Exportar rutas de usuarios
