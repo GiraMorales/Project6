88f33bd74a994dadfadf31c13fc6d902685e7ea6
GiraMorales <gira.morales@gamil.com>
2025-01-20 12:49:49 +0100

    otro retoque

--- src/api/models/users.js
+++ src/api/models/users.js
@@ -4,10 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' },
-    relatedUsers: [
-      { type: mongoose.Types.ObjectId, ref: 'users', unique: true }
-    ] // Array de relaciones
+    role: { type: String, enum: ['user', 'admin'], default: 'user' }
   },
   {
     timestamps: true,
